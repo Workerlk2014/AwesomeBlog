88691b279a00e5a8c6ffcb9ef052d2f66bdf5ef9
SeedPaths-User <lessdismissive@email.com>
2014-05-13 14:58:36 -0600

    first step
    a small step in the development of a blog website

---
+++ awesomeBlog/awesomeBlog/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace awesomeBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //step 1 set up database access
+        Models.myBlogEntities db = new Models.myBlogEntities();
+        // GET: /Home /
+
+        public ActionResult Index()
+        {
+            var list = db.Articles.OrderBy(x => x.ArticleDate);
+            return View(list);
+        }
+        public ActionResult Rating(int id)
+        {
+            var article = db.Articles.Where(x => x.ArticleID == id).First();
+            article.ArticlePositiveAppeal += 1;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Comment(int id, FormCollection values)
+        {
+            //making a new comment
+            var comments = new Models.Comment();
+            comments.ArticalID = id;
+            comments.CommentCreator = values["author"];
+            comments.CommentBody = values["body"];
+            comments.CommentDate = DateTime.Now;
+            comments.CommentRating = 0;
+            //add comment to the database
+            db.Comments.Add(comments);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ awesomeBlog/awesomeBlog/Models/blogValidation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace awesomeBlog.Models
+{
+    [MetadataType(typeof(ArticleMetadata))]
+    public partial class Article
+    {
+ 
+    }
+    public class ArticleMetadata
+    {
+        [Required(ErrorMessage = "you need to set a data")
+        , Display(Name= "Article Title")]
+        public string ArticleTitle;
+
+        [Required(ErrorMessage = "you need to set a data")
+        , Display(Name = "Your Message")]
+        public string ArticleBody;
+
+        [Required(ErrorMessage = "you need to set a data")
+        , Display(Name = "Date")]
+        public string ArticleDate;
+
+        [Required(ErrorMessage = "you need to set a data")
+        , Display(Name = "Your Name")]
+        public string ArticleCreator;
+
+        [Required(ErrorMessage = "you need to set a data")
+        , Display(Name = "Positive Rating")]
+        public string ArticlePositiveAppeal;
+
+        [Required(ErrorMessage = "you need to set a data")
+        , Display(Name = "Negative Rating")]
+        public string ArticleNegativeAppeal;
+
+        [Display(Name = "Picture Here or picture too long"), MaxLength(200)]
+        public string ArticlePicture;
+
+        [Display(Name = "Tags")]
+        public string ArticleTags;
+
+    }
+}
